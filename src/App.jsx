01e6e528d2e8c9b9ccef875a7cccd99c8f6a0980
JRealMe <jeremyloo82@yahoo.com>
2023-03-30 21:06:43 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
-import cadenaLogo from './assets/cadenaLogo.png';
+import jrealme from './assets/jrealme.png';
 
 //open ai config
 const configuration = new Configuration({
@@ -24,7 +24,7 @@ const App = () => {
     try {
       const completions = await apiClient.createCompletion({
         model: "text-davinci-003",
-        prompt: `Customize Your Prompt Here: ${prompt}`,
+        prompt: `Make a shoutout about: ${prompt} and add emojis to bring more fun and add multilanguage hashtags as well`,
         max_tokens: 880,
         temperature: 0.7,
         top_p: 1,
@@ -39,19 +39,19 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center px-4 sm:p-0">
-      <img src={cadenaLogo} alt="Cadena Logo" className="h-12 mb-10 mt-40" />
+    <div className="flex flex-col items-center justify-center px-4 sm:p-0 bg-black">
+      <img src={jrealme} alt="JRealMe Logo" className="h-20 mb-16 mt-40" style={{width: '400px', height: '200px'}}/>
       <form onSubmit={handleSubmit} className="w-full max-w-lg">
-        <div className="flex items-center border-b-2 border-indigo-600 py-2">
+        <div className="flex items-center border-b-2 border-cyan-900 py-2">
           <input
             className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
-            placeholder="Enter a topic you want to tweet about."
+            placeholder="Enter a topic you want to shoutout about.💬"
             value={prompt}
             onChange={handlePromptChange}
           />
           <button
-            className="flex-shrink-0 bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-sm border-4 text-white py-1 px-2 rounded"
+            className="flex-shrink-0 bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 text-sm border-3 text-white py-1 px-2 rounded"
             type="submit"
           >
             Generate
@@ -60,8 +60,8 @@ const App = () => {
       </form>
       {response && (
         <div className="w-full max-w-lg mt-4">
-          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <p className="text-gray-700 text-base">{response}</p>
+          <div className="bg-cyan-200 border-4 border-cyan-500 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <p className="text-gray-900 text-base">{response}</p>
           </div>
         </div>
       )}
